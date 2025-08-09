d1b852749c463b69e7645918fbcff7378addf019
Daniel88V <daniel12velo@gmail.com>
2025-08-08 21:00:21 -0600

    Actividad 14

--- Carrera.py
+++ Carrera.py
@@ -4,6 +4,11 @@ def agregar_corredor():
     for i in range(cant):
         print(f"Corredor #{i + 1}")
         while True:
-            dorsal = input("Ingrese el dorsal del corredor: ")
+            dorsal = int(input("Ingrese el dorsal del corredor: "))
             if dorsal in corredores:
                 print("Error, este dorsal ya fue asignado. Intente con otro número")
+            elif dorsal < 0:
+                print("Error, los dorsales deben de ser números positivos")
+            else:
+                break
+
